ccd150d4ed17f51f05e0740e8c340fa7d65a70d2
Nazir <>
2019-06-28 08:16:55 +0600

    commit

--- src/main.js
+++ src/main.js
@@ -6,3 +6,15 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log(`Service Worker registered! Scope: ${registration.scope}`);
+        })
+        .catch(err => {
+          console.log(`Service Worker registration failed: ${err}`);
+        });
+  });
+}
---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.html",
+    "js/*.js",
+    "css/*.css",
+    "img/*",
+  ],
+  "swDest": "dist/sw.js",
+  "swSrc": "public/sw.js"
+};
